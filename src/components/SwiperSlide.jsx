c2883dc8ddaa9771ba6a4f8cda417e5e81b1f9b8
GODZ-k <tanmaykhatri07726@gmail.com>
2024-08-31 16:37:40 +0530

    minimal update

--- src/components/SwiperSlide.jsx
+++ src/components/SwiperSlide.jsx
@@ -10,7 +10,8 @@ function SwiperSlide_comp({image , content , to}) {
       setIsOpen(false)
     }} className=" cursor-pointer relative w-60 h-56 rounded-tr-3xl rounded-bl-3xl overflow-hidden">
       <img className=" w-full h-full object-cover" src={image} alt="" />
-      <div   className=' absolute top-0 flex justify-center items-end right-0 w-full h-full bg-transparent'>
+    {content && (
+        <div   className=' absolute top-0 flex justify-center items-end right-0 w-full h-full bg-transparent'>
         <div className={`${isOpen ? 'h-24' : 'h-16 '} bg-[#0000008c] text-sm py-5 text-white w-full  transition-all ease-in-out duration-500 flex flex-col gap-5`}>
           <div>
             {content}
@@ -22,6 +23,7 @@ function SwiperSlide_comp({image , content , to}) {
           </Link>
         </div>
       </div>
+    )}
     </div>
   )
 }
--- src/pages/Home_page.jsx
+++ src/pages/Home_page.jsx
@@ -3,7 +3,12 @@ import DestinationCard from "@/components/DestinationCard";
 import Container from "@/components/Container";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay, Scrollbar } from "swiper/modules";
-import { Carousel ,CarouselContent , CarouselNext , CarouselPrevious} from "@/components/ui/carousel"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import "swiper/css";
 // import "swiper/css/navigation";;
 // import "swiper/css/pagination";
@@ -13,6 +18,134 @@ import CouponCard from "@/components/CouponCard";
 import { Link } from "react-router-dom";
 import ContainerHead from "@/components/ContainerHead";
 import { Button } from "@mui/material";
+
+const hotels = [
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101426/WhatsApp_Image_2024-08-31_at_10.39.13_AM_1_ydad1h.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101343/WhatsApp_Image_2024-08-31_at_10.33.12_AM_2_bbdyxd.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101345/WhatsApp_Image_2024-08-31_at_10.33.14_AM_1_lbjnq5.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101424/WhatsApp_Image_2024-08-31_at_10.39.10_AM_1_z0t3p0.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101425/WhatsApp_Image_2024-08-31_at_10.39.09_AM_1_bnrtey.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101427/WhatsApp_Image_2024-08-31_at_10.39.12_AM_3_fxgqod.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101343/WhatsApp_Image_2024-08-31_at_10.33.11_AM_t4bacp.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101425/WhatsApp_Image_2024-08-31_at_10.39.11_AM_1_cifu4w.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101430/WhatsApp_Image_2024-08-31_at_10.39.19_AM_bzhhyf.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101340/WhatsApp_Image_2024-08-31_at_10.33.07_AM_2_lrkwql.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101342/WhatsApp_Image_2024-08-31_at_10.33.09_AM_1_kjnnay.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101339/WhatsApp_Image_2024-08-31_at_10.33.05_AM_1_csvvn2.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101340/WhatsApp_Image_2024-08-31_at_10.33.06_AM_2_fslmrq.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101344/WhatsApp_Image_2024-08-31_at_10.33.09_AM_2_dqs6ij.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101340/WhatsApp_Image_2024-08-31_at_10.33.06_AM_1_oq8vpf.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101339/WhatsApp_Image_2024-08-31_at_10.33.05_AM_2_lwrweb.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101342/WhatsApp_Image_2024-08-31_at_10.33.09_AM_wtzay6.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101334/WhatsApp_Image_2024-08-31_at_10.37.14_AM_2_qck7nz.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101431/WhatsApp_Image_2024-08-31_at_10.39.20_AM_mucrck.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101340/WhatsApp_Image_2024-08-31_at_10.33.07_AM_3_kmstzt.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101334/WhatsApp_Image_2024-08-31_at_10.37.15_AM_2_xcy3n6.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101427/WhatsApp_Image_2024-08-31_at_10.39.13_AM_a4f43i.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101336/WhatsApp_Image_2024-08-31_at_10.37.20_AM_2_o2lga8.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101336/WhatsApp_Image_2024-08-31_at_10.37.19_AM_yxwvbb.jpg",
+    content: "",
+    to: ""
+  },
+  {
+    src: "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725101335/WhatsApp_Image_2024-08-31_at_10.37.16_AM_2_spqbxj.jpg",
+    content: "",
+    to: ""
+  }
+];
 
 function Home_page() {
   return (
@@ -119,11 +252,12 @@ function Home_page() {
         <div className=" flex justify-between gap-10 md:flex-row flex-col">
           <div className=" pt-5 w-full flex flex-col gap-3">
             <div className=" font-semibold sm:text-3xl text-2xl">
-            Be Ready for Our Exciting Transformation!
+              Be Ready for Our Exciting Transformation!
             </div>
             <div className=" text-gray-600">
-            We’re renovating, but you can plan ahead! Book early to enjoy our refreshed 
-            spaces and exclusive reopening offers. Stay tuned for special deals and updates on our grand reveal!
+              We’re renovating, but you can plan ahead! Book early to enjoy our
+              refreshed spaces and exclusive reopening offers. Stay tuned for
+              special deals and updates on our grand reveal!
             </div>
             <button className=" w-fit bg-black py-2 px-3 rounded-md text-white">
               Explore
@@ -133,53 +267,23 @@ function Home_page() {
             <Swiper
               modules={[Navigation, Autoplay, Scrollbar]}
               autoplay={{ delay: 2500 }}
-              slidesPerView={2}
+              slidesPerView={3}
               // centeredSlides={true}
               loop={true}
-              // spaceBetween={10}
+              spaceBetween={20}
               pagination={{
                 type: "fraction",
               }}
-  //             breakpoints={{
-  //   // when window width is >= 320px
-  //   320: {
-  //     slidesPerView: 2,
-  //   },
-  //   // when window width is >= 480px
-  //   480: {
-  //     slidesPerView: 2,
-  //   },
-  //   // when window width is >= 768px
-  //   768: {
-  //     slidesPerView: 2,
-  //   },
-  // }}
               className="mySwiper"
             >
-              <SwiperSlide>
-                <SwiperSlide_comp
-                  image={
-                    "https://res.cloudinary.com/dhb7hrvvn/image/upload/v1725084345/b4_z60pz1.jpg"
-                  }
-                  content={'Baris Hotel and Resort'}
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <SwiperSlide_comp
-                  image={
-                    "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725099509/WhatsApp_Image_2024-08-31_at_10.33.05_AM_hrlyju.jpg"
-                  }
-                  content={'Lavender Vill'}
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <SwiperSlide_comp
-                  image={
-                    "https://res.cloudinary.com/dlkzk9g9k/image/upload/v1725099598/WhatsApp_Image_2024-08-31_at_10.39.12_AM_ma3sjc.jpg"
-                  }
-                  content={'Hotel Driven'}
-                />
-              </SwiperSlide>
+              {hotels?.map((hotel,index) => (
+                <SwiperSlide key={index}>
+                  <SwiperSlide_comp
+                    image={hotel.src}
+                    // content={'Baris Hotel and Resort'}
+                  />
+                </SwiperSlide>
+              ))}
             </Swiper>
           </div>
         </div>
@@ -264,12 +368,14 @@ function Home_page() {
           <div className=" shadow-md p-5 rounded-3xl flex w-full flex-col-reverse sm:flex-row sm:gap-8 gap-5">
             <div className=" w-full sm:w-[50rem] flex flex-col gap-4">
               <div className=" text-xl font-semibold">
-              Tradition Meets Modern Luxury
+                Tradition Meets Modern Luxury
               </div>
               <div className=" text-[15px] text-gray-600">
-              Discover the Muss Collection at our hotel, where timeless elegance and modern comfort converge. Enjoy a stay that blends classic charm with contemporary style,
-               offering exceptional service and sophistication.
-                Redefine your travel experience with our unique fusion of tradition and luxury
+                Discover the Muss Collection at our hotel, where timeless
+                elegance and modern comfort converge. Enjoy a stay that blends
+                classic charm with contemporary style, offering exceptional
+                service and sophistication. Redefine your travel experience with
+                our unique fusion of tradition and luxury
               </div>
               <Link>
                 <button className=" bg-black py-3 px-4 rounded-lg text-white">
@@ -289,7 +395,11 @@ function Home_page() {
       </Container>
 
       {/* Reward Program */}
-      <Container id="rewards" className={"lg:w-full"} mainClassNamee={"px-0 sm:p-0 md:!p-0"}>
+      <Container
+        id="rewards"
+        className={"lg:w-full"}
+        mainClassNamee={"px-0 sm:p-0 md:!p-0"}
+      >
         <ContainerHead head={"Our Rewards Program"}>
           <div className=" relative w-full h-96">
             <img
@@ -300,9 +410,10 @@ function Home_page() {
             <div className=" absolute w-full h-full bg-[#00000063] flex items-center justify-center top-0 right-0">
               <div className="w-full sm:w-1/2 justify-start  flex flex-col gap-5 min-h-20 text-white p-3  ">
                 <div className=" sm:text-lg md:text-xl text-base">
-                Exclusive rewards and privileges for members, including complimentary stays, upgrades,
-                 special offers, and previews, with limitless possibilities to enhance your experience and enjoyment.
-                  Enjoy a world of benefits tailored just for you.
+                  Exclusive rewards and privileges for members, including
+                  complimentary stays, upgrades, special offers, and previews,
+                  with limitless possibilities to enhance your experience and
+                  enjoyment. Enjoy a world of benefits tailored just for you.
                 </div>
                 <div className=" text-3xl sm:text-5xl">Infinity Rewards</div>
                 <div className=" flex gap-4 items-center">
