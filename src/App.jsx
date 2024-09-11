48998aa32a01678d4489b3c86b19ecbe351ac6df
Tanmay/K and A Tech - MERN <tanmaykandatech-mern@192.168.1.10>
2024-09-11 10:24:10 +0530

    database

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Home_page from "./pages/Home_page";
-import Hotels_page from "./pages/hotels_page";
+import Hotels_page from "./pages/Hotels_page";
 
 function App() {
   return (
